e1cedaa88b1fa15127ed8d6908c160fad22f998f
hamza.hassine <hamza.hassine@supcom.tn>
2017-06-18 00:29:03 +0200

    Test debug

--- Sources/src/test/java/chat/TestDiffusion.java
+++ Sources/src/test/java/chat/TestDiffusion.java
@@ -24,16 +24,16 @@ public class TestDiffusion extends Scenario{
 
 		Server s0 = instanciateAServer("0");
 		sleep(500);
-		Server s1 = instanciateAServer("1 localhost 0");
-		sleep(500);
+//		Server s1 = instanciateAServer("1 localhost 0");
+//		sleep(500);
 		//Interceptor.setInterceptionEnabled(true);
-		sleep(500);
-		sleep(15000);
+//		sleep(500);
+		sleep(8000);
 		Client c0 = instanciateAClient(2050);
 		sleep(500);
-		Client c1 = instanciateAClient(2051);
+		Client c1 = instanciateAClient(2050);
 		sleep(500);
-		Client c2 = instanciateAClient(2051);
+		Client c2 = instanciateAClient(2050);
 		sleep(1000);
 		if (LOG_ON && TEST.isInfoEnabled()) {
 			TEST.info("starting the test of the diffusion algorithm...");
@@ -56,8 +56,8 @@ public class TestDiffusion extends Scenario{
 		emulateAnInputLineFromTheConsoleForAClient(c2, "quit");
 		sleep(100);
 		emulateAnInputLineFromTheConsoleForAServer(s0, "quit");
-		sleep(100);
-		emulateAnInputLineFromTheConsoleForAServer(s1, "quit");
+//		sleep(100);
+//		emulateAnInputLineFromTheConsoleForAServer(s1, "quit");
 		sleep(1000);
 
 	}
